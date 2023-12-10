cea3d80890842442f63d4254c9d8ac69e9ce9c09
Muhammad Saqib Ilyas <msaqib@gmail.com>
2023-12-10 12:31:05 +0500

    Complete with an animation

---
+++ app.js
@@ -0,0 +1,18 @@
+const tabs = document.querySelectorAll('.tabs-button')
+const line = document.querySelector('.line')
+const content = document.querySelectorAll('.content')
+
+const active = document.querySelector('.active')
+// line.style.width = active.offsetWidth + 'px'
+//line.style.left = active.offsetLeft + 'px'
+
+tabs.forEach( (tab, index) => {
+    tab.addEventListener( 'click', (event) => {
+        tabs.forEach( (tab) => tab.classList.remove('active'))
+        event.target.classList.add('active')
+        line.style.left = event.target.offsetLeft + 'px'
+        line.style.width = event.target.offsetWidth + 'px'
+        content.forEach( (para) => para.classList.remove('visible'))
+        content[index].classList.add('visible')
+    })
+})
